9bdca5ea91ed6b379429a138a0e28f5ffd784f4f
Ricardo Vallejo Sánchez <131738372+TodTete@users.noreply.github.com>
2026-03-12 10:02:25 -0600

    Initialize AOS and particlesJS in script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+AOS.init({
+duration:1000,
+once:true
+});
+
+particlesJS("particles-js",{
+particles:{
+number:{value:80},
+color:{value:"#ffffff"},
+shape:{type:"circle"},
+opacity:{value:0.5},
+size:{value:3},
+move:{enable:true,speed:1}
+}
+});
